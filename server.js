9683a9a9d00c3336c4ecf145dcf050c7d2b59248
johnfruscella <johnfruscella@gmail.com>
2020-01-27 23:57:23 -0500

    added dependencies and server.js file

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+mongoose.connect(process.env.DB_CONNECTION, options);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.listen(3000, () => console.log(`Server listening on port 3000`));
